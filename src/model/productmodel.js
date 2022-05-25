048f64c9606c515ea804006015cc1928e6778b52
agrawalsarthak4 <102967968+agrawalsarthak4@users.noreply.github.com>
2022-05-25 13:54:00 +0530

    Add files via upload

---
+++ src/model/productmodel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+
+    title: {string, mandatory, unique},
+    description: {string, mandatory},
+    price: {number, mandatory, valid number/decimal},
+    currencyId: {string, mandatory, INR},
+    currencyFormat: {string, mandatory, Rupee symbol},
+    isFreeShipping: {boolean, default: false},
+    productImage: {string, mandatory},  // s3 link
+    style: {string},
+    availableSizes: {array of string, at least one size, enum["S", "XS","M","X", "L","XXL", "XL"]},
+    installments: {number},
+    deletedAt: {Date, when the document is deleted}, 
+    isDeleted: {boolean, default: false},
+    createdAt: {timestamp},
+    updatedAt: {timestamp},
+    {timestamps: true });
+
+    module.exports = mongoose.model('User', userSchema)
+
+    // ctrl+alt+4
---
+++ src/route/route.js
@@ -0,0 +1,23 @@
+const express = require("express"); //import express
+const router = express.Router(); //used express to create route handlers
+const {createUser,login,getUserProfile,updateUser} = require("../controller/usercontroller")
+const {authenticate}=require("../middleware/middleware")
+
+//user APIs
+router.post('/register', createUser)
+router.post('/login', login)
+router.get('/user/:userId/profile', authenticate,getUserProfile)
+router.put("/user/:userId/profile", authenticate,updateUser);
+
+
+
+/----------------if api is invalid OR wrong URL-------------------------/
+router.all("/**", function (req, res) {
+    res
+      .status(404)
+      .send({ status: false, msg: "The api you request is not available" });
+  });
+
+
+//export router
+module.exports = router;
